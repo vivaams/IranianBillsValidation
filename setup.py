16d10e991d4545a2553b83dcb8eb55a02fb7f7a5
vivaams <vivaams@yahoo.com>
2021-09-27 19:33:17 +0330

    update

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="iranian-bills-validation",
-    version="0.0.1",
+setup(
+    name="iranian_bills_validation",
+    version='0.0.1',
+    license='MIT',
     author="Amin Morakabi Sabet",
     author_email="vivaams@yahoo.com",
-    description="This module checks and validates your bills and returns the required information",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     url="https://github.com/vivaams/iranian_bills_validation",
-    project_urls={
-        "Bug Tracker": "https://github.com/vivaams/iranian_bills_validation/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3",
+    keywords='iranian bills validation',
+    install_requires=[
+          'jdatetime',
+      ],
+
 )
