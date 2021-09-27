b4f3eb27f73daac15cc2dcb230e3c1ea56527561
vivaams <vivaams@yahoo.com>
2021-09-27 18:57:41 +0330

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iranian_bills_validation",
-    version="1.0",
+    version="1.0.1",
     author="Amin Morakabi Sabet",
     author_email="vivaams@yahoo.com",
     description="This module checks and validates your bills and returns the required information",
