f387ce1bf08d56c041f919d3a20a75169c544c68
Amin Morakabi Sabet <vivaams@yahoo.com>
2021-09-30 11:44:16 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="iranian_bills_validation",
-    version="1.1",
-    author="Amin Morakabi Sabet",
-    author_email="vivaams@yahoo.com",
-    description="This module checks and validates your bills and returns the required information",
+setup(
+    name='iranian_bills_validation',
+    packages=['iranian_bills_validation'],
+    version='1.1.2',
+    license='MIT',
+    description='This module checks and validates your bills and returns the required information',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/vivaams/iranian_bills_validation",
+    author='Amin Morakabi Sabet',
+    author_email='vivaams@yahoo.com',
+    url='https://github.com/vivaams/iranian_bills_validation',
     project_urls={
         "Bug Tracker": "https://github.com/vivaams/iranian_bills_validation/issues",
     },
+    keywords=['bills', 'validation'],
     install_requires=[
-          'jdatetime',
-    ],
+            'jdatetime',
+        ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3",
 )
